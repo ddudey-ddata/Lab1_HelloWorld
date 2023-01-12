3c622278b6c0bf490fab06fff4da7af8315e9b28
Daryl Dudey <daryljdudey@googlemail.com>
2023-01-12 19:54:14 +0000

    Idea Project, compiles and runs both publisher and subscriber

--- src/main/java/com/diffusion/training/lab1/HelloWorldPublisher.java
+++ src/main/java/com/diffusion/training/lab1/HelloWorldPublisher.java
@@ -16,62 +16,62 @@ import com.pushtechnology.diffusion.client.topics.details.TopicType;
 
 public class HelloWorldPublisher {
 
-	Session session;
-	TopicControl topicControl;
-	TopicUpdate topicUpdate;
+    Session session;
+    TopicControl topicControl;
+    TopicUpdate topicUpdate;
 
-	private static final Logger LOG = LoggerFactory.getLogger(HelloWorldPublisher.class);
+    private static final Logger LOG = LoggerFactory.getLogger(HelloWorldPublisher.class);
 
-	public HelloWorldPublisher() {
-		connect("ws://localhost:8080", "admin", "password");
-		try {
-			this.publishToTopic("my/first/topic");
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (ExecutionException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (TimeoutException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+    public HelloWorldPublisher() {
+        connect("ws://localhost:8080", "admin", "password");
+        try {
+            this.publishToTopic("my/first/topic");
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (TimeoutException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
 
-	}
+    }
 
-	public void connect(String url, String username, String password) {
+    public void connect(String url, String username, String password) {
 
-		session = Diffusion.sessions().principal(username).password(password).open(url);
+        session = Diffusion.sessions().principal(username).password(password).open(url);
 
-	}
+    }
 
-	public void publishToTopic(String topicPath) throws InterruptedException, ExecutionException, TimeoutException {
-		// Get the TopicControl and TopicUpdate feature
-		final TopicControl topicControl = session.feature(TopicControl.class);
+    public void publishToTopic(String topicPath) throws InterruptedException, ExecutionException, TimeoutException {
+        // Get the TopicControl and TopicUpdate feature
+        final TopicControl topicControl = session.feature(TopicControl.class);
 
-		final TopicUpdate topicUpdate = session.feature(TopicUpdate.class);
+        final TopicUpdate topicUpdate = session.feature(TopicUpdate.class);
 
-		// Create an int64 topic 'foo/counter'
-		final CompletableFuture<TopicControl.AddTopicResult> future = topicControl.addTopic(topicPath,
-				TopicType.STRING);
+        // Create an int64 topic 'foo/counter'
+        final CompletableFuture<TopicControl.AddTopicResult> future = topicControl.addTopic(topicPath,
+                TopicType.STRING);
 
-		// Wait for the CompletableFuture to complete
-		future.get(10, TimeUnit.SECONDS);
+        // Wait for the CompletableFuture to complete
+        future.get(10, TimeUnit.SECONDS);
 
-		// Update the topic
-		for (long i = 0; i < 1000; ++i) {
+        // Update the topic
+        for (long i = 0; i < 1000; ++i) {
 
-			// Use the non-exclusive updater to update the topic without locking it
-			LOG.info("Publishing to Topic");
-			topicUpdate.set(topicPath, String.class, "Hello World -" + i);
+            // Use the non-exclusive updater to update the topic without locking it
+            LOG.info("Publishing to Topic");
+            topicUpdate.set(topicPath, String.class, "Hello World -" + i);
 
-			Thread.sleep(1000);
-		}
-	}
+            Thread.sleep(1000);
+        }
+    }
 
-	public static void main(String[] args) {
-		new HelloWorldPublisher();
+    public static void main(String[] args) {
+        new HelloWorldPublisher();
 
-	}
+    }
 
 }
--- src/main/java/com/diffusion/training/lab1/HelloWorldSubscriber.java
+++ src/main/java/com/diffusion/training/lab1/HelloWorldSubscriber.java
@@ -11,54 +11,54 @@ import com.pushtechnology.diffusion.client.topics.details.TopicSpecification;
 
 public class HelloWorldSubscriber {
 
-	Session session;
-	Topics topics;
-	private static final Logger LOG = LoggerFactory.getLogger(HelloWorldSubscriber.class);
+    Session session;
+    Topics topics;
+    private static final Logger LOG = LoggerFactory.getLogger(HelloWorldSubscriber.class);
 
-	public HelloWorldSubscriber() {
-		connect("ws://localhost:8080");
-		this.subscribeToTopic("my/first/topic");
+    public HelloWorldSubscriber() {
+        connect("ws://localhost:8080");
+        this.subscribeToTopic("my/first/topic");
 
-	}
+    }
 
-	public void connect(String url) {
+    public void connect(String url) {
 
-		session = Diffusion.sessions().open(url);
-	}
+        session = Diffusion.sessions().open(url);
+    }
 
-	public void subscribeToTopic(String topicPath) {
-		// Get the Topics feature to subscribe to topics
-		topics = session.feature(Topics.class);
+    public void subscribeToTopic(String topicPath) {
+        // Get the Topics feature to subscribe to topics
+        topics = session.feature(Topics.class);
 
-		// Add a new stream for topic
-		topics.addStream(topicPath, String.class, new ValueStreamPrintLn());
+        // Add a new stream for topic
+        topics.addStream(topicPath, String.class, new ValueStreamPrintLn());
 
-		// Subscribe to the topic
-		topics.subscribe(topicPath).whenComplete((voidResult, exception) -> {
-			if (exception != null) {
-				LOG.info("subscription failed", exception);
-			}
-		});
+        // Subscribe to the topic
+        topics.subscribe(topicPath).whenComplete((voidResult, exception) -> {
+            if (exception != null) {
+                LOG.info("subscription failed", exception);
+            }
+        });
 
-		// Wait for a minute while the stream prints updates
-		try {
-			Thread.sleep(60000);
-		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
+        // Wait for a minute while the stream prints updates
+        try {
+            Thread.sleep(60000);
+        } catch (InterruptedException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
 
-	public static void main(String[] args) {
-		new HelloWorldSubscriber();
+    public static void main(String[] args) {
+        new HelloWorldSubscriber();
 
-	}
+    }
 
-	private static class ValueStreamPrintLn extends ValueStream.Default<String> {
-		@Override
-		public void onValue(String topicPath, TopicSpecification specification, String oldValue, String newValue) {
-			System.out.println(topicPath + ":   " + newValue);
-		}
-	}
+    private static class ValueStreamPrintLn extends ValueStream.Default<String> {
+        @Override
+        public void onValue(String topicPath, TopicSpecification specification, String oldValue, String newValue) {
+            System.out.println(topicPath + ":   " + newValue);
+        }
+    }
 
 }
